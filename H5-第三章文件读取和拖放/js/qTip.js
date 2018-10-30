5fe60169a939b076f6b204d79b2048b06227de64
qq1332783374 <1332783374@qq.com>
2018-10-30 11:48:23 +0800

    HTML5学习

---
+++ H5-第三章文件读取和拖放/js/qTip.js
@@ -0,0 +1,12 @@
+;(function($){
+	$.fn.tools = function(options){
+		var defaults = {
+			
+		}
+		var options = $.extend(defaults,options);
+		this.each(function(){
+			var _this = $(this);
+			
+		})
+	}
+})(jQuery)
